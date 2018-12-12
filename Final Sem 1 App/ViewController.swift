82026f23b39b316ab5eae7d61604499acc7c9582
Brandon Kim <bkim2192@jh-mba158-16.local>
2018-12-12 14:23:54 -0600

    Initial Commit

---
+++ Final Sem 1 App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Sem 1 App
+//
+//  Created by Brandon Kim on 12/12/18.
+//  Copyright © 2018 Brandon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
