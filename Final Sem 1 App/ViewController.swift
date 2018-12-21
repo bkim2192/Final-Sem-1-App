ecae5e34def1661ac592e4f06c6d38fcfe5b9f29
Brandon Kim <bkim2192@jh-mba158-16.local>
2018-12-21 09:26:58 -0600

    finished

--- Final Sem 1 App/ViewController.swift
+++ Final Sem 1 App/ViewController.swift
@@ -8,13 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var enterNumber: UITextField!
+    @IBOutlet weak var Answer: UILabel!
+    //Source: https://www.youtube.com/watch?v=YkF0mby3fMA //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+}
+    
+    func convert2(decimal: Int) -> String {
+        var n = 0, c = 0, k:[String] = [], fs: String = ""
+        
+        n = decimal
+        
+        while n > 0 {
+            c = n % 2
+            n = n / 2
+            k.append("\(c)")
+        }
+        
+       for iy in k.reversed() {
+            fs += "\(iy)"
+       
+        }
+        return fs
+    
+    
     }
+    
+    
+    @IBAction func BaseTwo(_ sender: Any) {
+       Answer.text = convert2(decimal: (enterNumber.text! as NSString).integerValue)
+        enterNumber.resignFirstResponder()
+        
+        
+        
+    }
+    
+
+
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*if number2 == 1 {
+Answer.text = "1"
+}
+if number2 == 2 {
+    Answer.text = "10"
+}
+if number2 == 3 {
+    Answer.text = "11"
+}
+if number2 > 3, number2 < 6 {
+    var b3 = Number! - 2^2
+    var b4 = b3 - 2^1
+    var b5 = b4 - 2^0
+    let array1 = [b3, b4, b5]
+//var string3 = Int(String(array1))
+// Answer.text = string3 */
 
